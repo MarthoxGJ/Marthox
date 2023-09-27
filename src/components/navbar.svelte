5420148752ab1dd0f79f5c569dd74a95806fb94f
Mateo Gregory <43354689+MarthoxGJ@users.noreply.github.com>
2023-09-27 13:17:25 -0500

    Add navbar (#3)

---
+++ src/components/navbar.svelte
@@ -0,0 +1,27 @@
+<script>
+    /**
+     * @type {Array<{name: string, url: string}>}
+     */
+    export let routes = [{
+        name: "Home",
+        url: "/"
+    }, {
+        name: "About",
+        url: "/about"
+    }, {
+        name: "Contact",
+        url: "/contact"}];
+</script>
+
+<nav>
+    <ul>
+        <li>
+            Marthox
+        </li>
+        {#each routes as route}
+            <li>
+                <a href={route.url}>{route.name}</a>
+            </li>
+        {/each}
+    </ul>
+</nav>
