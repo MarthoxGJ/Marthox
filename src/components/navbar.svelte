07dfc8299f62dbe083abe4c60019626b60a46bcc
Mateo Gregory <43354689+MarthoxGJ@users.noreply.github.com>
2023-09-30 12:21:14 -0500

    Style navbar (#4)

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -14,14 +14,50 @@
 </script>
 
 <nav>
+    <li class="nav-logo">
+        Marthox
+    </li>
     <ul>
-        <li>
-            Marthox
-        </li>
         {#each routes as route}
-            <li>
+            <li class="nav-item">
                 <a href={route.url}>{route.name}</a>
             </li>
         {/each}
     </ul>
+    <li class="nav-language-selector">
+        Language
+    </li>
 </nav>
+
+<style lang="scss">
+    nav {
+        background-color: #333;
+        color: #fff;
+        display: flex;
+        height: 3rem;
+        justify-content: space-between;
+    }
+
+    ul {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    li{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 5rem;    
+    }
+
+    .nav-item {
+        &:hover {
+            background-color: #555;
+        }
+    }
+
+    a {
+        color: #fff;
+        text-decoration: none;
+    }
+</style>
