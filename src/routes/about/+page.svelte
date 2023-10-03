74803d584382716707f9c23019ef88e156dfac28
Mateo Gregory <43354689+MarthoxGJ@users.noreply.github.com>
2023-10-02 23:37:17 -0500

    Create about structure (#8)

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to About/SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="about-page-container">
+    <div class="body-container">
+        <h1>I'm Mateo Gregory Jimenez</h1>
+        <p>I'm a systems engineer from <a href="https://www.univalle.edu.co/" target="_blank">Univalle</a>, graduated in 2021 with a meritorious mention for his work</p>
+    </div>
+    
+    <div class="image-container">
+        <img class="profile-photo" alt="Mateo Gregory Jimenez in closeup" src="https://scontent.fclo11-1.fna.fbcdn.net/v/t39.30808-6/321246259_475342334791062_1986002789912976877_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=UgcrgM9BenEAX-Q64Gw&_nc_ht=scontent.fclo11-1.fna&oh=00_AfDJFvBxkGE-Xhz-Vn9J8vK-k3kXdHpjB0q3-voHvvh-qA&oe=6521389D"/>
+    </div>
+
+</div>
+
+
+
+<style lang="scss">
+    .about-page-container{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        position: absolute;
+        z-index: -1;
+        width: 100vw;
+        height: 100vh;
+        top: 0;
+        left: 0;
+    }
+
+    .body-container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding-left: 4rem;
+        width: 50vw;
+        height: 100%;
+    }
+
+    .image-container{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 50vw;
+        height: 100%;
+    }
+
+    .profile-photo {
+        width: 80%;
+        border-radius: 50%;
+    }
+</style>
